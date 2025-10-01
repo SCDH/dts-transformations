42d654e19df4926141e800198cdcdc4aa2ea08ac
Christian Lück <christian.lueck@uni-muenster.de>
2025-10-02 00:35:31 +0200

    introduces a commom property file for Maven and Ant

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-JAVAOPTS="-Ddebug=true -Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dxspec.version=${xspec.version}"
+JAVAOPTS="-Ddebug=true -Dorg.slf4j.simpleLogger.defaultLogLevel=info"
 
 CP=$CLASSPATH
 for j in ${project.build.directory}/lib/*.jar; do
